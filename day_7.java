c1ed443c7e4c85adc0104172d495b600e38703b9
Amanpreet16 <preetamank343@gmail>
2023-08-04 14:29:49 +0530

    LIS based questions

--- day_7.java
+++ day_7.java
@@ -4,6 +4,11 @@ public class day_7 {
      /*
       * important string questions 
       * Longest Increasing sussequence (leet code 300)
+      *longest decreasing sunsequence
+      * Longest bitonic sussequence
+      * longest bitonic subsequence reverse pattern 
+      *maximum sum increasing subsequense
+      * maximum sum bitoinc subsequence
       * maximum number of bridges
       * minimum number of deletion to make array sorted
       *
@@ -14,11 +19,11 @@ public class day_7 {
         public int lengthOfLIS(int[] nums) {
             int n=nums.length;
             int[] dp=new int[n];
-            Arrays.fill(dp,1);
             int max=0;
             
             for(int i=0;i<n;i++){
-                for(int j=0;j<=i;j++){
+                dp[i]=1;
+                for(int j=i-1;j>=0;j--){
                     if(nums[j]<nums[i]){
                         dp[i]=Math.max(dp[i],dp[j]+1);
                   
@@ -29,6 +34,172 @@ public class day_7 {
             return max;
         }
 
+
+
+        
+      // longest Decreasing Subsequence
+      public static int LDS(int[] arr){
+        int n=arr.length;
+         int[] dp=new int[n];
+        int maxLen=0;
+        for(int i=n-1;i>=0;i--){
+            dp[i]=1;
+            for(int j=i+1;j<n;j++){
+                if(arr[i]>arr[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+            }
+            maxLen=Math.max(maxLen,dp[i]);
+        }
+     return  maxLen;
+      }
+
+      // longest Bitonic subsequence
+     public static int[] LIS_LR(int[] nums) {
+            int n=nums.length;
+            int[] dp=new int[n];
+            for(int i=0;i<n;i++){
+                dp[i]=1;
+                for(int j=0;j<=i;j++){
+                    if(nums[j]<nums[i]){
+                        dp[i]=Math.max(dp[i],dp[j]+1);
+                  
+                    }
+                }
+                      
+            }
+            return dp;
+        }
+       // lis right to left == LDS
+        public static int[] LIS_RL(int[] nums){
+        int n=nums.length;
+         int[] dp=new int[n];
+        for(int i=n-1;i>=0;i--){
+            dp[i]=1;
+            for(int j=i+1;j<n;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+            }
+        }
+     return  dp;
+      }
+      public static int longestBitonicsubsequence(int[] nums){
+        int[] lis=LIS_LR(nums);
+        int[] lds=LIS_RL(nums);
+        int n=nums.length, maxLen=0;
+        for(int i=0;i<nums.length;i++){
+            maxLen=Math.max(maxLen,lis[i]+lds[i]-1);
+        }
+        return maxLen;
+      }
+
+      //=======================================================================================================//
+     // bitonic subsequence reverse order
+     // LDS left to right
+      public static int[] LDS_LR(int[] nums) {
+            int n=nums.length;
+            int[] dp=new int[n];
+            for(int i=0;i<n;i++){
+                dp[i]=1;
+                for(int j=i-1;j>=0;j--){
+                    if(nums[j] >nums[i]){
+                        dp[i]=Math.max(dp[i],dp[j]+1);
+                  
+                    }
+                }
+                      
+            }
+            return dp;
+        }
+       // LDS right to left == LIS
+        public static int[] LDS_RL(int[] nums){
+        int n=nums.length;
+         int[] dp=new int[n];
+        for(int i=n-1;i>=0;i--){
+            dp[i]=1;
+            for(int j=i+1;j<n;j++){
+                if(nums[i]<nums[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+            }
+        }
+     return  dp;
+      }
+      public static int longestBitonicsubsequence_2(int[] nums){
+        int[] lds=LDS_LR(nums);
+        int[] lis=LDS_RL(nums);
+        int n=nums.length, maxLen=0;
+        for(int i=0;i<nums.length;i++){
+            maxLen=Math.max(maxLen,lis[i]+lds[i]-1);
+        }
+        return maxLen;
+      }
+
+
+      // maximum icreasing sum Subsequence
+      // geeks for geek https://practice.geeksforgeeks.org/problems/maximum-sum-increasing-subsequence4749
+      public int maxSumIS(int nums[], int n) {
+        int[] dp=new int[n];
+        int maxSum=0;
+        for(int i=0;i<n;i++){
+            dp[i]=nums[i];
+            for(int j=i-1;j>=0;j--){
+                if(nums[i]>nums[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+nums[i]);
+              
+                }
+            }
+            maxSum=Math.max(maxSum,dp[i]);
+                  
+        }
+        return maxSum;
+    } 
+
+    // maximum sum bitonic subsequence
+    // geeks for geek
+    public static int maxSumBS(int nums[], int n)
+    {
+      int[] lis=LIS_LR(nums);
+        int[] lds=LIS_RL(nums);
+       int maxSum=0;
+        for(int i=0;i<n;i++){
+            maxSum=Math.max(maxSum,(lis[i]+lds[i])-nums[i]);
+        }
+        return maxSum;
+      }
+      
+          public static int[] LIS_LR(int[] nums) {
+            int n=nums.length;
+            int[] dp=new int[n];
+            for(int i=0;i<n;i++){
+                dp[i]=nums[i];
+                for(int j=0;j<=i;j++){
+                    if(nums[j]<nums[i]){
+                        dp[i]=Math.max(dp[i],dp[j]+nums[i]);
+                  
+                    }
+                }
+                      
+            }
+            return dp;
+        }
+       // lis right to left == LDS
+        public static int[] LIS_RL(int[] nums){
+        int n=nums.length;
+         int[] dp=new int[n];
+        for(int i=n-1;i>=0;i--){
+            dp[i]=nums[i];
+            for(int j=i+1;j<n;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+nums[i]);
+                }
+            }
+        }
+     return  dp;
+      }
+
+
      // maximum number of bridges
       public static int maximumBridges(int[][] points){
         int n=points.length;
@@ -38,6 +209,7 @@ public class day_7 {
         int[] dp=new int[n];
         int maxLen=0;
         for(int i=0;i<n;i++){
+            dp[i]=1;
             for(int j=i-1;j>=0;j--){
                 if(points[i][1]>points[j][1]){
                     dp[i]=Math.max(dp[i],dp[j]+1);
@@ -48,12 +220,14 @@ public class day_7 {
      return  maxLen;
       }
 
+
       // minimum number of deletion to make array sorted
      public static int mimdeletion(int[] arr){
          int n=arr.length;
             int[] dp=new int[n];
             int max=0;
             for(int i=0;i<n;i++){
+                dp[i]=1;
                 for(int j=i-1;j>=0;j--){
                     if(arr[i]>=arr[j]){
                         dp[i]=Math.max(dp[i],dp[j]+1);
